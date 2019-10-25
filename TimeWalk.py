c377dfceb39e8f7d51f4995095f8eb9923e7d8aa
Geonhee Oh <goni@Geonhees-MBP.cable.rcn.com>
2019-10-24 22:44:49 -0500

    update

--- TimeWalk.py
+++ TimeWalk.py
@@ -6,15 +6,15 @@ from scipy.interpolate import Rbf, InterpolatedUnivariateSpline
 from itertools import groupby
 from operator import itemgetter
 from scipy import optimize
+import os,sys
 
 verbose = False
 path = "inputHd5/"
 filename = "laser.hd5"
-
+outputD='results'
+os.system('mkdir -p %s'%outputD)
 f = h5py.File(path+filename,'r')
-
 dataset = f['C0']
-
 #print(dataset.shape[0])
 rng = f.attrs['Waveform Attributes'].tolist()
 if verbose :
@@ -79,48 +79,6 @@ t0 = t0_*dt
 t1 = t1_*dt
 data = t1-t0
 
-#print(t1)
-#print(t0)
-#print(t1-t0)
-#sns.distplot(t1-t0, hist=True, kde=True,
-#             color = 'darkblue',
-#             hist_kws={'edgecolor':'black'},
-#             kde_kws={'linewidth': 4})
-#np.mean(t1-t0, axis=None, dtype=None, out=None, keepdims=False)
-
-from scipy.stats import norm
-mu, std = norm.fit(data)
-# Plot the histogram.
-plt.hist(data, bins=20, density=True, alpha=0.6, color='b')
-xmin, xmax = plt.xlim()
-xnorm = np.linspace(xmin, xmax, 100)
-p = norm.pdf(xnorm, mu, std)
-
-
-#FWHMx = np.linspace(10, 110, 1000)
-#green = xnorm(FWHMx, 50, 10)
-#pink = xnorm(FWHMx, 60, 10)
-#spline = UnivariateSpline(data, xnorm-np.max(xnorm)/2, s=0)
-#r1, r2 = spline.roots() # find the roots
-from scipy.interpolate import UnivariateSpline
-spline = UnivariateSpline(x,ynorm-50,s=0)
-fwhm = abs(spline.roots()[1]-spline.roots()[0])
-
-plt.plot(xnorm, p, c='r')
-plt.hist(data, bins=20)
-##labels and save
-plt.title("t1-t0 (1000evts)")
-plt.xlabel("time(s)")
-plt.ylabel("Events")
-plt.savefig("fig1.pdf")
-
-#hypothesis tests null is normal dist. Alternate hypo is not null.. 
-from scipy.stats import shapiro #shapiro
-stat, p = shapiro(data)
-print('Statistics=%.3f, p=%.3f' % (stat, p))
-from statsmodels.graphics.gofplots import qqplot #qqplot quantile mapping with real data
-qqplot(data, line='s')
-
 ##### For plotting #####
 #iset = 600 #for random test whether the t1 and t0 are fine
 #y_org = dataset[1, npoints*iset:npoints*(iset+1)]*ymult
@@ -144,21 +102,63 @@ qqplot(data, line='s')
 #
 #f = interp1d(yy, xx, kind='cubic') ## interpolate to fit between 2 points
 
+#Normal distribution
+from scipy.stats import norm
+mu, std = norm.fit(data) #Gaussian fit
+# Plot the histogram.
+plt.hist(data, bins=20, density=True, alpha=0.6, color='b', label='t1-t0')
+xmin, xmax = plt.xlim()
+xnorm = np.linspace(xmin, xmax, 100)
+p = norm.pdf(xnorm, mu, std)
+#FWHM
+from scipy.interpolate import UnivariateSpline
+import pylab as pl
+spline = UnivariateSpline(xnorm, p-np.max(p)/2, s=0)
+fwhm = abs(spline.roots()[1]-spline.roots()[0])
+print("Full Width Half Maximum:", fwhm)
+pl.axvspan(spline.roots()[0], spline.roots()[1], facecolor='g', alpha=0.5, label='FWHM')
+print("Sigma:",std)
+norm.fit(data)
+pl.plot(xnorm, p, c='r', label='Fit')
+pl.legend(loc='best')
+pl.savefig("%s/fig1_w_FWHM.pdf" %outputD)
+pl.show()
+
+##labels and save
+plt.hist(data, bins=20, label='t1-t0')
+plt.title("t1-t0 (1000evts)")
+plt.xlabel("time(s)")
+plt.ylabel("Events")
+plt.legend(loc='best')
+plt.savefig("%s/fig_wo_FWHM.pdf" %outputD)
+#hypothesis tests null is normal dist. Alternate hypo is not null..
+from scipy.stats import shapiro #shapiro
+stat, p = shapiro(data)
+print('Statistics=%.3f, p=%.3f' % (stat, p))
+from statsmodels.graphics.gofplots import qqplot #qqplot quantile mapping with real data
+qqplot(data, line='s')
+plt.savefig("%s/hypothesis_tests.pdf" %outputD)
 
 plt.figure(figsize=(12,8))
-plt.scatter(x, y0, label='input pulse', color='r')
-plt.scatter(x, y1, label='pre-amp puls')
-plt.plot(x, interp_fn0(x), color='black')
-plt.plot(x, interp_fn1(x), color='black')
+plt.subplot(211)
+plt.scatter(x, y0, label='laser', color='r')
+plt.plot(x, interp_fn0(x), color='black', label='interpolated by Newton Raphson method')
 plt.grid(True)
-plt.title("TEST iset")
+plt.title("Ch1")
+plt.ylabel("amplitude")
+plt.hlines(y=y_t0, xmin=t0/dt-50, xmax=t0/dt+50, color='magenta', label='50% of maxV (laser)')
+plt.vlines(x=t0_[999], ymin=-0.5, ymax=0.5, label='t0', color='g')
+plt.legend(loc='best')
+
+plt.subplot(212)
+plt.scatter(x, y1, label='pre-amp pulse')
+plt.plot(x, interp_fn1(x), color='black', label='interpolated by Newton Raphson method')
+plt.grid(True)
+plt.title("Ch2")
 plt.xlabel("time")
 plt.ylabel("amplitude")
-plt.hlines(y=y_t1, xmin=700, xmax=900, label='20% of maxV (pre-amp)', color='b')
+plt.hlines(y=y_t1, xmin=t1/dt-50, xmax=t1/dt+50, label='20% of maxV (pre-amp)', color='b')
 plt.vlines(x=t1_[999], ymin=-0.5, ymax=0.5, label='t1', color='purple')
-#plt.xlines(, xmin=-400, xmax=600, label='t0', color='g')
-plt.vlines(x=t0/dt, ymin=-0.5, ymax=0.5, label='t0', color='g')
 plt.legend(loc='best')
-plt.savefig("fig2.pdf")
-
+plt.savefig("%s/figs.pdf" %outputD)
 plt.show()
